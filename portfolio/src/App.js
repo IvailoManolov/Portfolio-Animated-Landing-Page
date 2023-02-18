8ac5a71a8a26a0606d7f3e1a58ef4ddeb315fdc8
IvailoManolov <ivomoreras@abv.bg>
2023-02-18 15:04:05 +0200

    navigation-bar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,8 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="container">
+      <nav>
+        <div className='logo'> LEAD EN<b>G</b>INEER </div>
+
+        <ul className='nav-items'>
+          <li><a href='#'> HOME </a></li>
+          <li><a href='#'> ABOUT </a></li>
+          <li><a href='#'> SKILLS </a></li>
+          <li><a href='#'> BLOG </a></li>
+        </ul>
+
+      </nav>
     </div>
   );
 }
