8f224530937f6763d0fb91f05e8e6b16ddea7dd7
IvailoManolov <ivomoreras@abv.bg>
2023-02-18 18:34:47 +0200

    landing design finished

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,7 @@
 
 import './App.css';
+import personPhoto from "./images/result.png"
+import splashScreen from "./images/splash.png"
 
 function App() {
   return (
@@ -15,6 +17,27 @@ function App() {
         </ul>
 
       </nav>
+
+      <div className='wrapper'>
+        <div className='cols cols0'>
+          <span className='topline'>Hello</span>
+          <h1>I'm <span className='multiText'> Coder</span></h1>
+          <p>Lorem ipsum dolor sit amet, consecttetur adipising elit.</p>
+          <div className='btns'>
+          <button> Download CV </button>
+          <button> Hire Me </button>
+        </div>
+        </div>
+        
+        
+
+        <div className='cols cols1'>
+          <div className='imgbox'>
+            <img src={splashScreen} id = "splash"/>
+            <img src={personPhoto}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
